bbb449b1c25f8c3d789bede21275a4636617096c
Henry <elpadredelcordero@gmail.com>
2025-10-25 19:51:56 -0500

    fix de comunicación de kafka

--- src/presentation/controllers/event_controllers/kafka_event_controller.py
+++ src/presentation/controllers/event_controllers/kafka_event_controller.py
@@ -2,15 +2,12 @@ import asyncio
 import json
 import logging
 from typing import Any
-
 from aiokafka import AIOKafkaConsumer
 from pydantic import ValidationError
-
-from domain.enums.document_type import DocumentType
 from domain.models.states.document_contract_state import DocumentContractState
 from infrastructure.bootstrap.container import build_workflow
 from infrastructure.config.app_settings import KafkaSettings, get_app_settings
-from presentation.dtos.requests.process_document import ProcessDocumentRequest
+from presentation.dtos.requests.process_document import ProcessDocumentRequest, ProcessDocument
 
 app_logger = logging.getLogger("app.environment")
 
@@ -64,11 +61,11 @@ class KafkaEventController:
                     document_requests: list[ProcessDocumentRequest] = []
                     for m in msgs:
                         text = m.value.decode("utf-8", errors="ignore")
-                        data = json.loads(text) if text else {}
-                        print(data)
-                        process_document: ProcessDocumentRequest = (
-                            ProcessDocumentRequest.model_validate(data, by_alias=True)
-                        )
+                        # Por negocio se decidió que solo debe procesarse un documento a la vez
+                        one_document_data = json.loads(text) if text else {}  # Esto representa un solo documento
+                        print(one_document_data)
+                        one_document: ProcessDocument = ProcessDocument.model_validate(**one_document_data)
+                        process_document = ProcessDocumentRequest(documents=[one_document])
                         document_requests.append(process_document)
                     tasks.append(
                         asyncio.create_task(
@@ -77,6 +74,8 @@ class KafkaEventController:
                     )
                 if tasks:
                     await asyncio.gather(*tasks, return_exceptions=True)
+        except Exception as e:
+            print(f"error: {str(e)}")
         finally:
             await c.stop()
 
@@ -86,38 +85,20 @@ class KafkaEventController:
 
         async with self._sem:
             try:
-                documents_by_type: dict[DocumentType, list[DocumentContractState]] = (
-                    dict()
-                )
-                for item in document_requests:
-                    document_contract_state = DocumentContractState(
-                        record_id=item.record_id,
-                        parent_id=item.parent_id,
-                        key=item.key,
-                        session_id=item.session_id,
-                        document_type=item.document_type,
-                        period_month=item.period_month,
-                        period_year=item.period_year,
-                    )
-                    if item.document_type not in documents_by_type:
-                        documents_by_type[item.document_type] = [
-                            document_contract_state
-                        ]
-                    else:
-                        documents_by_type[item.document_type].append(
-                            document_contract_state
+                documents_to_wf: list[DocumentContractState] = []
+                for doc in document_requests:
+                    for item in doc.documents:
+                        document_contract_state = DocumentContractState(
+                            record_id=item.record_id,
+                            parent_id=item.parent_id,
+                            key=item.key,
+                            session_id=item.session_id,
+                            document_type=item.document_type,
+                            period_month=item.period_month,
+                            period_year=item.period_year,
                         )
-
-                print(documents_by_type)
-                for [document_type, documents] in documents_by_type.items():
-                    app_logger.info(
-                        f"Ejecutando flow de: {document_type} - {len(documents)}"
-                    )
-                    result = await self._wf.execute(
-                        document_type=document_type, documents=documents
-                    )
-                return {"status": "success"}
-
+                        documents_to_wf.append(document_contract_state)
+                await self._wf.execute(documents=documents_to_wf)
             except ValidationError as e:
                 app_logger.exception(f"Error de validación: {str(e)}")
             except Exception as e:
